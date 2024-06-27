b234100a65b3b1d024b6929e7b95ffb16fcdae6f
RSkoi <31830382+RSkoi@users.noreply.github.com>
2024-06-27 23:19:06 +0200

    init

---
+++ LightShafts.Shared/LightShaftsPlugin.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+
+namespace LightShaftsPlugin
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class LightShaftsPlugin : BaseUnityPlugin
+    {
+        internal const string PLUGIN_GUID = "RSkoi.robcupisz.LightShafts";
+        internal const string PLUGIN_NAME = "LightShafts";
+        internal const string PLUGIN_VERSION = "1.0.0";
+
+        internal static LightShaftsPlugin _instance;
+        internal static ManualLogSource _logger;
+
+        #region materials for shaders
+        private static Material _matColorFilter;
+        private static Material _matCoord;
+        private static Material _matDepth;
+        private static Material _matDepthBreaks;
+        private static Material _matFinalInterpolation;
+        private static Material _matInterpolateAlongRays;
+        private static Material _matRaymarch;
+        private static Material _matSamplePositions;
+        #endregion
+
+        #region shaders
+        internal static Shader _ColorFilter;
+        internal static Shader _Coord;
+        internal static Shader _Depth;
+        internal static Shader _DepthBreaks;
+        internal static Shader _FinalInterpolation;
+        internal static Shader _InterpolateAlongRays;
+        internal static Shader _Raymarch;
+        internal static Shader _SamplePositions;
+        #endregion
+
+        internal void Awake()
+        {
+            _instance = this;
+            _logger = Logger;
+        }
+
+        internal void Start()
+        {
+            LoadShaderResources();
+            CacheShaders();
+        }
+
+        private void LoadShaderResources()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.lightshafts.unity3d");
+            byte[] buffer = new byte[stream.Length];
+            stream.Read(buffer, 0, buffer.Length);
+
+            AssetBundle ab              = AssetBundle.LoadFromMemory(buffer);
+            _matColorFilter             = ab.LoadAsset<Material>("ls_ColorFilter");
+            _matCoord                   = ab.LoadAsset<Material>("ls_Coord");
+            _matDepth                   = ab.LoadAsset<Material>("ls_Depth");
+            _matDepthBreaks             = ab.LoadAsset<Material>("ls_DepthBreaks");
+            _matFinalInterpolation      = ab.LoadAsset<Material>("ls_FinalInterpolation");
+            _matInterpolateAlongRays    = ab.LoadAsset<Material>("ls_InterpolateAlongRays");
+            _matRaymarch                = ab.LoadAsset<Material>("ls_Raymarch");
+            _matSamplePositions         = ab.LoadAsset<Material>("ls_SamplePositions");
+
+            stream.Close();
+        }
+
+        private void CacheShaders()
+        {
+            _ColorFilter            = _matColorFilter.shader;
+            _Coord                  = _matCoord.shader;
+            _Depth                  = _matDepth.shader;
+            _DepthBreaks            = _matDepthBreaks.shader;
+            _FinalInterpolation     = _matFinalInterpolation.shader;
+            _InterpolateAlongRays   = _matInterpolateAlongRays.shader;
+            _Raymarch               = _matRaymarch.shader;
+            _SamplePositions        = _matSamplePositions.shader;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# LightShafts
+
+Volumetric Light Shafts for the KK/KKS studio, lazy port of [Light Shafts](https://github.com/robcupisz/LightShafts). Requires [Component Util](https://github.com/RSkoi/ComponentUtil).
+
+Light Shafts are computationally expensive. Try to keep resolutions and sample counts to a minimum.
+
+Note that releases include zipmod with Unity's built-in Standard shaders which exposes them to [Material Editor](https://github.com/IllusionMods/KK_Plugins?tab=readme-ov-file#materialeditor).
+
+## How-To-Use
+
+0. Add a `Directional` or `Spot` light to the studio workspace.
+1. Select the light and toggle ComponentUtil's UI.
+2. Add the `LightShaftsEffect` with the ComponentAdder window to the selected `GameObject`, it has to be next to a `Component` named `Light`.
+3. Adjust properties and fields of the `LightShaftsEffect` to your liking.
+
+## Known Quirks & Issues
+
+- If you add the effect to a `GameObject` without a `Light`, the effect will auto-add one for you, but ComponentUtil will not keep track of it across scene save / load.
+- Not all shaders are supported, meaning not all objects will be picked up by the Light Shafts. Generally, Light Shafts will only react to Standard-adjacent shaders, such as Unity's built-in Standard or IBL_shader. You can sometimes hack your way around this limitation by duplicating a material, setting the shader to Standard and 'hiding' it behind the original material.
